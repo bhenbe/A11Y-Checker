2de8a007f42981dafed1e885f2ee15aca659cd97
bhenbe <contact@bhen.be>
2018-12-27 10:37:35 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/*** requires ***/
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars'); 
+const bodyParser = require('body-parser');  
+const url = require('url');  
+const querystring = require('querystring');  
+const pa11y = require('pa11y');
+
+/*** config ***/
+var app = express();
+var options = {
+    dotfiles: 'ignore',
+    extensions: ['htm', 'html'],
+    index: false
+    };
+app.use(express.static(path.join(__dirname, 'public'), options));
+app.set('port', process.env.PORT || 3000);
+
+/*** handlebars template engine ***/
+app.engine('handlebars', hbs({defaultLayout: 'main'})); 
+app.set('view engine', 'handlebars');
+
+/*** routes ***/
+app.get('/', async function (req, res) {
+    var formurl = 'http://' + req.headers.host;
+
+    if (req.query.websiteurl) {
+        try{
+            await pa11y(req.query.websiteurl).then(results => {
+                res.render('accessibility', {name: req.query.websiteurl, result: results, url: formurl});
+            });
+        } catch(error){
+            res.render('error', {name: req.query.websiteurl, message: error, url: formurl});
+        }
+    } else {
+        res.render('accessibility', {name: '', result: '', url: formurl});
+    }
+});
+
+/*** listen ***/
+app.listen(app.get('port'),  function () {
+    console.log('Hello express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});
